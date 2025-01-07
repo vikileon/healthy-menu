110653d2f0bbc15eb4614cbbfd243915324b554c
Daria Timoshevskaya <vileonika@gmail.com>
2025-01-07 20:43:07 +0300

    Added links to different cuisines

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import Pages from './pages/Pages'
+import Pages from './pages/Pages';
+import Category from './components/Category';
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Pages />
+      <BrowserRouter>
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
