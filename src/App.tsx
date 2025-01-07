7cd3dd00e33999fe067821b0d7c0e0bdc2956d71
Daria Timoshevskaya <vileonika@gmail.com>
2025-01-07 22:39:14 +0300

    Edited cuisines links; added search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Pages from './pages/Pages';
 import Category from './components/Category';
-import { BrowserRouter } from 'react-router-dom'
+import Search from './components/Search';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,32 +4,68 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 function Category() {
-  return (
-    <List>
-        <NavLink to={'/cuisine/Italian'}>
-            <FaPizzaSlice />
-            <h4>Italian</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/American'}>
-            <FaHamburger />
-            <h4>American</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Thai'}>
-            <GiNoodles />
-            <h4>Thai</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Japanese'}>
-            <GiChopsticks />
-            <h4>Japanese</h4>
-        </NavLink>
-    </List>
-  );
+    return (
+        <List>
+            <StyledLink to={'/cuisine/Italian'}>
+                <FaPizzaSlice />
+                <h4>Italian</h4>
+            </StyledLink>
+            <StyledLink to={'/cuisine/American'}>
+                <FaHamburger />
+                <h4>American</h4>
+            </StyledLink>
+            <StyledLink to={'/cuisine/Thai'}>
+                <GiNoodles />
+                <h4>Thai</h4>
+            </StyledLink>
+            <StyledLink to={'/cuisine/Japanese'}>
+                <GiChopsticks />
+                <h4>Japanese</h4>
+            </StyledLink>
+        </List>
+    );
 };
 
 const List = styled.div`
     display: flex;
     justify-content: center;
     margin: 2rem 0rem;
+`
+
+const StyledLink = styled(NavLink)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 2rem;
+    text-decoration: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    height: 6rem;
+    width: 6rem;
+    cursor: pointer;
+    transform: scale(0.8);
+
+    h4 {
+        color: white;
+        font-size: 0.8rem;
+    }
+
+    svg {
+        color: white;
+        font-size: 1.5rem;
+    }
+
+    &.active {
+        background: linear-gradient(to right, #f27121, #e94057);
+
+        svg{
+            color: white;
+        }
+        h4{
+            color: white;
+        }
+    }
 `
 
 export default Category;
